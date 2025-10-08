69c59d7dd7e37c3dcd948c3b838ca3f7ce630bf7
vodoleikin <vodoleikin@yandex.ru>
2025-10-08 21:43:39 +0300

    08.10.2025. 21.40. v16. All tests passed. Final version of ya_note/notes/tests/test_content.py, readded ya_note/notes/tests/conftest.py.

--- ya_note/notes/tests/conftest.py
+++ ya_note/notes/tests/conftest.py
@@ -10,18 +10,20 @@ class BaseTestCase(TestCase):
 
     @classmethod
     def setUpTestData(cls):
-        
-        cls.userFirst = User.objects.create_user(
-            username='UserFirst',
-            password='UserFirstpassword')
-        
+
+        # Создание первого авторизованного пользователя
+        cls.userFirstAuthorized = User.objects.create_user(
+            username='UserFirstAuthorized',
+            password='UserFirstAuthorizedPassword')
+
         cls.clientFirst = Client()
-        cls.clientFirst.force_login(cls.userFirst)
-                
-        cls.note = Note.objects.create(
-            title='Тестовая заметка',
-            text='Содержание тестовой заметки',
-            author=cls.userFirst)
+        cls.clientFirst.force_login(cls.userFirstAuthorized)
+      
+        # Создание первой заметки первого авторизованного пользователя
+        cls.first_note_userFirstAuthorized = Note.objects.create(
+            title='Тестовая заметка 1 авторизованного пользователя 1',
+            text='Содержание тестовой заметки 1 авторизованного пользователя 1',
+            author=cls.userFirstAuthorized)
 
         cls.url_test_anonymous_access_notes_home = reverse('notes:home')
         cls.url_test_anonymous_access_users_login = reverse('users:login')
@@ -31,18 +33,18 @@ class BaseTestCase(TestCase):
             'notes:list': reverse('notes:list'),
             'notes:success': reverse('notes:success'),
             'notes:add': reverse('notes:add'),
-            'notes:detail': reverse('notes:detail', args=[cls.note.id]),
-            'notes:edit': reverse('notes:edit', args=[cls.note.id]),
-            'notes:delete': reverse('notes:delete', args=[cls.note.id])
+            'notes:detail': reverse('notes:detail', args=[cls.first_note_userFirstAuthorized.id]),
+            'notes:edit': reverse('notes:edit', args=[cls.first_note_userFirstAuthorized.id]),
+            'notes:delete': reverse('notes:delete', args=[cls.first_note_userFirstAuthorized.id])
         }
 
         cls.urls_test_authenticated_access = {
             'notes:list': reverse('notes:list'),
             'notes:success': reverse('notes:success'),
             'notes:add': reverse('notes:add'),
-            'notes:detail': reverse('notes:detail', args=[cls.note.slug]),
-            'notes:edit': reverse('notes:edit', args=[cls.note.slug]),
-            'notes:delete': reverse('notes:delete', args=[cls.note.slug])
+            'notes:detail': reverse('notes:detail', args=[cls.first_note_userFirstAuthorized.slug]),
+            'notes:edit': reverse('notes:edit', args=[cls.first_note_userFirstAuthorized.slug]),
+            'notes:delete': reverse('notes:delete', args=[cls.first_note_userFirstAuthorized.slug])
         }
 
         cls.urls_test_public_pages = {
@@ -53,14 +55,85 @@ class BaseTestCase(TestCase):
 
     def setUp(self):
 
-        self.userSecond = User.objects.create_user(
+        # Создание второго авторизованного пользователя
+        self.userSecondAuthorized = User.objects.create_user(
             username='UserSecond',
             password='UserSecondPassword'
         )
-
         self.clientSecond = Client()
-        self.clientSecond.force_login(self.userSecond)
+        self.clientSecond.force_login(self.userSecondAuthorized)
 
+        # Создание третьего неавторизованного пользователя (Аноним)
+        self.clientThirdAnonimus = Client()
+
+        # Создание второй заметки второго авторизованного пользователя
+        self.second_note_userSecondAuthorized = Note.objects.create(
+            title='Тестовая заметка 1 авторизованного пользователя 2',
+            text='Содержание тестовой заметки 1 авторизованного пользователя 2',
+            author=self.userSecondAuthorized)
+
+
+
+# # ----------
+# from django.contrib.auth.models import User
+
+# from django.test import Client, TestCase
+# from django.urls import reverse
+
+# from notes.models import Note
+
+
+# class BaseTestCase(TestCase):
+
+#     @classmethod
+#     def setUpTestData(cls):
         
+#         cls.userFirst = User.objects.create_user(
+#             username='UserFirst',
+#             password='UserFirstpassword')
+        
+#         cls.clientFirst = Client()
+#         cls.clientFirst.force_login(cls.userFirst)
+                
+#         cls.note = Note.objects.create(
+#             title='Тестовая заметка',
+#             text='Содержание тестовой заметки',
+#             author=cls.userFirst)
 
+#         cls.url_test_anonymous_access_notes_home = reverse('notes:home')
+#         cls.url_test_anonymous_access_users_login = reverse('users:login')
+#         cls.url_test_anonymous_access_users_logout = reverse('users:logout')
 
+#         cls.urls_test_anonymous_access = {
+#             'notes:list': reverse('notes:list'),
+#             'notes:success': reverse('notes:success'),
+#             'notes:add': reverse('notes:add'),
+#             'notes:detail': reverse('notes:detail', args=[cls.note.id]),
+#             'notes:edit': reverse('notes:edit', args=[cls.note.id]),
+#             'notes:delete': reverse('notes:delete', args=[cls.note.id])
+#         }
+
+#         cls.urls_test_authenticated_access = {
+#             'notes:list': reverse('notes:list'),
+#             'notes:success': reverse('notes:success'),
+#             'notes:add': reverse('notes:add'),
+#             'notes:detail': reverse('notes:detail', args=[cls.note.slug]),
+#             'notes:edit': reverse('notes:edit', args=[cls.note.slug]),
+#             'notes:delete': reverse('notes:delete', args=[cls.note.slug])
+#         }
+
+#         cls.urls_test_public_pages = {
+#             'users:signup': reverse('users:signup'),
+#             'users:login': reverse('users:login'),
+#             'users:logout': reverse('users:logout')
+#         }
+
+#     def setUp(self):
+
+#         self.userSecond = User.objects.create_user(
+#             username='UserSecond',
+#             password='UserSecondPassword'
+#         )
+
+#         self.clientSecond = Client()
+#         self.clientSecond.force_login(self.userSecond)
--- ya_note/notes/tests/test_content.py
+++ ya_note/notes/tests/test_content.py
@@ -1,73 +1,44 @@
-# Стандартные импорты
+from http import HTTPStatus
 import unittest
 
-# Импорты Django (сторонние)
-from django.contrib.auth.models import User
-from django.test import Client, TestCase
-from django.urls import reverse
-
-# Импорты проекта
-from notes.models import Note
+from notes.forms import NoteForm
+from .conftest import BaseTestCase
 
 
-class ContentTests(TestCase):
+class ContentTests(BaseTestCase):
 
-    def setUp(self):
-        """Создание тестовых пользователей."""
-        self.user1 = User.objects.create_user(
-            username='User1',
-            password='User1_123')
-        self.user2 = User.objects.create_user(
-            username='User2',
-            password='User2_123')
+    def test_user_note_in_list_and_notes_isolation(self):
+        # Проверка передачи заметки в context.
+        # Проверка изоляции заметок разных пользователей.
+        authenticated_users = (
+            (self.clientFirst, self.first_note_userFirstAuthorized),
+            (self.clientSecond, self.second_note_userSecondAuthorized)
+        )
+        for user, note in authenticated_users:
+            for user_, note_ in authenticated_users[::-1]:
+                if user == user_:
+                    continue
+                with self.subTest():
+                    response = user.get(
+                        self.urls_test_authenticated_access['notes:list'])
+                    self.assertEqual(response.status_code, HTTPStatus.OK)
+                    self.assertIn(note, response.context['object_list'])
+                    self.assertNotIn(note_, response.context['object_list'])
 
-        """Создание тестовые заметки."""
-        self.first_note = Note.objects.create(
-            title='Заметка 1',
-            text='Содержание заметки 1',
-            author=self.user1)
-        self.second_note = Note.objects.create(
-            title='Заметка 2',
-            text='Содержание заметки 2',
-            author=self.user2)
-
-        """Создание клиента и авторизация пользователя."""
-        self.client = Client()
-        self.client.force_login(self.user1)
-
-    def test_note_in_list(self):
-        """Проверка передачи заметки в context."""
-        response = self.client.get(reverse('notes:list'))
-        self.assertEqual(response.status_code, 200)
-        self.assertIn(self.first_note, response.context['object_list'])
-        self.assertNotIn(self.second_note, response.context['object_list'])
-
-    def test_user_notes_isolation(self):
-        """Проверка изоляции заметок разных пользователей."""
-        # Проверка заметок для первого пользователя
-        response = self.client.get(reverse('notes:list'))
-        self.assertEqual(response.status_code, 200)
-        self.assertIn(self.first_note, response.context['object_list'])
-        self.assertNotIn(self.second_note, response.context['object_list'])
-        # Авторизация и проверка второго пользователя
-        self.client.force_login(self.user2)
-        response = self.client.get(reverse('notes:list'))
-        self.assertEqual(response.status_code, 200)
-        self.assertNotIn(self.first_note, response.context['object_list'])
-        self.assertIn(self.second_note, response.context['object_list'])
-
-    def test_form_in_create_view(self):
-        """Проверка передачи формы на страницу создания заметки."""
-        response = self.client.get(reverse('notes:add'))
-        self.assertEqual(response.status_code, 200)
-        self.assertIsNotNone(response.context['form'])
-
-    def test_form_in_edit_view(self):
-        """Проверка передачи формы на страницу редактирования заметки."""
-        response = self.client.get(reverse(
-            'notes:edit', args=[self.first_note.slug]))
-        self.assertEqual(response.status_code, 200)
-        self.assertIsNotNone(response.context['form'])
+    def test_form_in_create_and_edit_view(self):
+        # Проверка передачи формы на страницу создания
+        # страницу и редактирования заметок.
+        urls = (
+            self.urls_test_authenticated_access['notes:add'],
+            self.urls_test_authenticated_access['notes:edit']
+        )
+        for url in urls:
+            with self.subTest():
+                response = self.clientFirst.get(url)
+                self.assertEqual(response.status_code, HTTPStatus.OK)
+                self.assertIn('form', response.context)
+                self.assertIsNotNone(response.context['form'])
+                self.assertIsInstance(response.context['form'], NoteForm)
 
 
 if __name__ == '__main__':
@@ -84,22 +55,59 @@ if __name__ == '__main__':
 
 
 
+# # ----------++++++++++----------
+# # Стандартные импорты
+# import unittest
+
+# # Импорты Django (сторонние)
+# from django.contrib.auth.models import User
+# from django.test import Client, TestCase
+# from django.urls import reverse
+
+# # Импорты проекта
+# from notes.models import Note
+# from .conftest import BaseTestCase
 
 
+# class ContentTests(BaseTestCase):
+
+#     def test_note_in_list(self):
+#         """Проверка передачи заметки в context."""
+#         response = self.clientFirst.get(reverse('notes:list'))
+#         self.assertEqual(response.status_code, 200)
+#         self.assertIn(self.first_note_userFirstAuthorized, response.context['object_list'])
+#         self.assertNotIn(self.second_note_userSecondAuthorized, response.context['object_list'])
+
+#     def test_user_notes_isolation(self):
+#         """Проверка изоляции заметок разных пользователей."""
+#         # Проверка заметок для первого пользователя
+#         response = self.clientFirst.get(reverse('notes:list'))
+#         self.assertEqual(response.status_code, 200)
+#         self.assertIn(self.first_note_userFirstAuthorized, response.context['object_list'])
+#         self.assertNotIn(self.second_note_userSecondAuthorized, response.context['object_list'])
+#         # Авторизация и проверка второго пользователя
+#         # self.client.force_login(self.user2)
+#         response = self.clientSecond.get(reverse('notes:list'))
+#         self.assertEqual(response.status_code, 200)
+#         self.assertNotIn(self.first_note_userFirstAuthorized, response.context['object_list'])
+#         self.assertIn(self.second_note_userSecondAuthorized, response.context['object_list'])
+
+#     def test_form_in_create_view(self):
+#         """Проверка передачи формы на страницу создания заметки."""
+#         response = self.clientFirst.get(reverse('notes:add'))
+#         self.assertEqual(response.status_code, 200)
+#         self.assertIsNotNone(response.context['form'])
+
+#     def test_form_in_edit_view(self):
+#         """Проверка передачи формы на страницу редактирования заметки."""
+#         response = self.clientFirst.get(reverse(
+#             'notes:edit', args=[self.first_note_userFirstAuthorized.slug]))
+#         self.assertEqual(response.status_code, 200)
+#         self.assertIsNotNone(response.context['form'])
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+# if __name__ == '__main__':
+#     unittest.main()
 
 
 
@@ -179,3 +187,4 @@ if __name__ == '__main__':
 
 # if __name__ == '__main__':
 #     unittest.main()
+
