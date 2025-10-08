8213b0a4674b53a4dec4f5b5123ebc735a4cd545
vodoleikin <vodoleikin@yandex.ru>
2025-10-08 22:46:28 +0300

    08.10.2025. 22.45. v17. Not all tests passed. Draft ya_note/notes/tests/test_logic.py, readded ya_note/notes/tests/conftest.py.

--- ya_note/notes/tests/conftest.py
+++ ya_note/notes/tests/conftest.py
@@ -1,8 +1,6 @@
 from django.contrib.auth.models import User
-
 from django.test import Client, TestCase
 from django.urls import reverse
-
 from notes.models import Note
 
 
@@ -22,7 +20,7 @@ class BaseTestCase(TestCase):
         # Создание первой заметки первого авторизованного пользователя
         cls.first_note_userFirstAuthorized = Note.objects.create(
             title='Тестовая заметка 1 авторизованного пользователя 1',
-            text='Содержание тестовой заметки 1 авторизованного пользователя 1',
+            text='Содержание заметки 1 авторизованного пользователя 1',
             author=cls.userFirstAuthorized)
 
         cls.url_test_anonymous_access_notes_home = reverse('notes:home')
@@ -69,8 +67,98 @@ class BaseTestCase(TestCase):
         # Создание второй заметки второго авторизованного пользователя
         self.second_note_userSecondAuthorized = Note.objects.create(
             title='Тестовая заметка 1 авторизованного пользователя 2',
-            text='Содержание тестовой заметки 1 авторизованного пользователя 2',
+            text='Содержание заметки 1 авторизованного пользователя 2',
             author=self.userSecondAuthorized)
+
+        self.quantity_notes = Note.objects.count()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # ----------++++++++++----------
+# from django.contrib.auth.models import User
+# from django.test import Client, TestCase
+# from django.urls import reverse
+# from notes.models import Note
+
+
+# class BaseTestCase(TestCase):
+
+#     @classmethod
+#     def setUpTestData(cls):
+
+#         # Создание первого авторизованного пользователя
+#         cls.userFirstAuthorized = User.objects.create_user(
+#             username='UserFirstAuthorized',
+#             password='UserFirstAuthorizedPassword')
+
+#         cls.clientFirst = Client()
+#         cls.clientFirst.force_login(cls.userFirstAuthorized)
+      
+#         # Создание первой заметки первого авторизованного пользователя
+#         cls.first_note_userFirstAuthorized = Note.objects.create(
+#             title='Тестовая заметка 1 авторизованного пользователя 1',
+#             text='Содержание тестовой заметки 1 авторизованного пользователя 1',
+#             author=cls.userFirstAuthorized)
+
+#         cls.url_test_anonymous_access_notes_home = reverse('notes:home')
+#         cls.url_test_anonymous_access_users_login = reverse('users:login')
+#         cls.url_test_anonymous_access_users_logout = reverse('users:logout')
+
+#         cls.urls_test_anonymous_access = {
+#             'notes:list': reverse('notes:list'),
+#             'notes:success': reverse('notes:success'),
+#             'notes:add': reverse('notes:add'),
+#             'notes:detail': reverse('notes:detail', args=[cls.first_note_userFirstAuthorized.id]),
+#             'notes:edit': reverse('notes:edit', args=[cls.first_note_userFirstAuthorized.id]),
+#             'notes:delete': reverse('notes:delete', args=[cls.first_note_userFirstAuthorized.id])
+#         }
+
+#         cls.urls_test_authenticated_access = {
+#             'notes:list': reverse('notes:list'),
+#             'notes:success': reverse('notes:success'),
+#             'notes:add': reverse('notes:add'),
+#             'notes:detail': reverse('notes:detail', args=[cls.first_note_userFirstAuthorized.slug]),
+#             'notes:edit': reverse('notes:edit', args=[cls.first_note_userFirstAuthorized.slug]),
+#             'notes:delete': reverse('notes:delete', args=[cls.first_note_userFirstAuthorized.slug])
+#         }
+
+#         cls.urls_test_public_pages = {
+#             'users:signup': reverse('users:signup'),
+#             'users:login': reverse('users:login'),
+#             'users:logout': reverse('users:logout')
+#         }
+
+#     def setUp(self):
+
+#         # Создание второго авторизованного пользователя
+#         self.userSecondAuthorized = User.objects.create_user(
+#             username='UserSecond',
+#             password='UserSecondPassword'
+#         )
+#         self.clientSecond = Client()
+#         self.clientSecond.force_login(self.userSecondAuthorized)
+
+#         # Создание третьего неавторизованного пользователя (Аноним)
+#         self.clientThirdAnonimus = Client()
+
+#         # Создание второй заметки второго авторизованного пользователя
+#         self.second_note_userSecondAuthorized = Note.objects.create(
+#             title='Тестовая заметка 1 авторизованного пользователя 2',
+#             text='Содержание тестовой заметки 1 авторизованного пользователя 2',
+#             author=self.userSecondAuthorized)
+        
 
 
 
--- ya_note/notes/tests/test_logic.py
+++ ya_note/notes/tests/test_logic.py
@@ -1,120 +1,246 @@
+from http import HTTPStatus
 import unittest
 from django.urls import reverse
 from django.contrib.auth.models import User
 from django.test import Client, TestCase
 from notes.models import Note
 from pytils.translit import slugify
+from .conftest import BaseTestCase
 
 
-class NoteLogicTests(TestCase):
+class NoteLogicTests(BaseTestCase):
 
-    def setUp(self):
-        """Создание тестовых пользователей."""
-        self.user1 = User.objects.create_user(
-            username='User1', password='User1_123')
-        self.user2 = User.objects.create_user(
-            username='User2', password='User2_123')
-
-        """Создание клиента и авторизация пользователя."""
-        self.client = Client()
-        self.client.force_login(self.user1)
-
-        """Создание тестовую заметку."""
-        self.note = Note.objects.create(
-            title='Тест',
-            text='Содержание теста',
-            author=self.user1)
+    SECOND_TEST_NOTE_TITLE = 'Тестовая заметка 2 авторизованного пользователя 2'
+    SECOND_TEST_NOTE_TEXT = 'Содержание заметки 2 авторизованного пользователя 2'
+    TITLE_CHANGED_NOTE = 'Измененная заметка'
+    TEXT_CHANGED_NOTE = 'Новое содержание'
 
     def test_create_note_authenticated(self):
         """Тест на создание заметки авторизованным пользователем."""
         # Проверяем начальное количество заметок
-        self.assertEqual(Note.objects.count(), 1)
-        response = self.client.post(reverse('notes:add'), data={
-            'title': 'Новая заметка',
-            'text': 'Содержание новой заметки'})
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(Note.objects.count(), 2)
+
+        self.assertEqual(Note.objects.count(), self.quantity_notes)
+
+        response = self.clientSecond.post(
+            self.urls_test_authenticated_access['notes:add'],
+            data={
+                'title': self.SECOND_TEST_NOTE_TITLE,
+                'text': self.SECOND_TEST_NOTE_TEXT})
+        self.assertEqual(response.status_code, HTTPStatus.FOUND)
+        self.assertEqual(Note.objects.count(), self.quantity_notes + 1)
         # Проверяем создание новой заметки
         new_note = Note.objects.last()
-        self.assertEqual(new_note.title, 'Новая заметка')
-        self.assertEqual(new_note.text, 'Содержание новой заметки')
-        self.assertEqual(new_note.author, self.user1)
-        # print('Good')
+        self.assertEqual(new_note.title, self.SECOND_TEST_NOTE_TITLE)
+        self.assertEqual(new_note.text, self.SECOND_TEST_NOTE_TEXT)
+        self.assertEqual(new_note.author, self.userSecondAuthorized)
 
-    def test_create_note_anonymous(self):
-        """Тест на невозможность создания заметки анонимным пользователем."""
-        anon_client = Client()
-        response = anon_client.post(reverse('notes:add'), {
-            'title': 'Новая заметка',
-            'text': 'Содержание новой заметки'})
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(Note.objects.count(), 1)
+    # def test_create_note_anonymous(self):
+    #     """Тест на невозможность создания заметки анонимным пользователем."""
+    #     anon_client = Client()
+    #     response = anon_client.post(reverse('notes:add'), {
+    #         'title': 'Новая заметка',
+    #         'text': 'Содержание новой заметки'})
+    #     self.assertEqual(response.status_code, 302)
+    #     self.assertEqual(Note.objects.count(), 1)
 
-    def test_unique_slug(self):
-        """Тест на уникальность slug."""
-        # Попытка создать заметку с тем же slug
-        with self.assertRaises(Exception):
-            Note.objects.create(
-                title=self.note.title,
-                text='Другое содержание',
-                author=self.user1,
-                slug=self.note.slug)
+    # def test_unique_slug(self):
+    #     """Тест на уникальность slug."""
+    #     # Попытка создать заметку с тем же slug
+    #     with self.assertRaises(Exception):
+    #         Note.objects.create(
+    #             title=self.note.title,
+    #             text='Другое содержание',
+    #             author=self.user1,
+    #             slug=self.note.slug)
 
-    def test_auto_slug(self):
-        """Тест на автоматическое формирование slug."""
-        response = self.client.post(reverse('notes:add'), {
-            'title': 'Тестовая заметка',
-            'text': 'Содержание'})
-        self.assertEqual(response.status_code, 302)
-        new_note = Note.objects.last()
-        self.assertEqual(new_note.slug, slugify('тестовая-заметка'))
+    # def test_auto_slug(self):
+    #     """Тест на автоматическое формирование slug."""
+    #     response = self.client.post(reverse('notes:add'), {
+    #         'title': 'Тестовая заметка',
+    #         'text': 'Содержание'})
+    #     self.assertEqual(response.status_code, 302)
+    #     new_note = Note.objects.last()
+    #     self.assertEqual(new_note.slug, slugify('тестовая-заметка'))
 
     def test_edit_own_note(self):
         """Тест на редактирование своей заметки."""
-        response = self.client.post(reverse(
-            'notes:edit', args=[self.note.slug]), {
-            'title': 'Измененная заметка',
-            'text': 'Новое содержание'})
-        self.assertEqual(response.status_code, 302)
-        self.note.refresh_from_db()
-        self.assertEqual(self.note.title, 'Измененная заметка')
+        response = self.clientFirst.post(
+            self.urls_test_authenticated_access['notes:edit'], {
+                'title': self.TITLE_CHANGED_NOTE,
+                'text': self.TEXT_CHANGED_NOTE})
+        self.assertEqual(response.status_code, HTTPStatus.FOUND)
+        self.first_note_userFirstAuthorized.refresh_from_db()
+        self.assertEqual(
+            self.first_note_userFirstAuthorized.title,
+            self.TITLE_CHANGED_NOTE)
 
     def test_edit_foreign_note(self):
         """Тест на невозможность редактирования чужой заметки."""
-        # Создаем заметку для второго пользователя
-        foreign_note = Note.objects.create(
-            title='Чужая заметка',
-            text='Содержание чужой заметки',
-            author=self.user2)
-        response = self.client.post(reverse(
-            'notes:edit', args=[foreign_note.slug]), {
-            'title': 'Попытка изменения'})
-        self.assertEqual(response.status_code, 404)  # Запрещено
+        response = self.clientSecond.post(
+            self.urls_test_authenticated_access['notes:edit'],
+            {'title': 'Попытка изменения'})
+        self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
 
-    def test_delete_own_note(self):
-        """Тест на удаление своей заметки."""
-        response = self.client.post(reverse(
-            'notes:delete', args=[self.note.slug]))
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(Note.objects.count(), 0)
+    # def test_delete_own_note(self):
+    #     """Тест на удаление своей заметки."""
+    #     response = self.clientFirst.post(
+    #         self.urls_test_authenticated_access['notes:delete'])
+    #     self.assertEqual(response.status_code, 302)
+    #     self.assertEqual(Note.objects.count(), 0)
 
     def test_delete_foreign_note(self):
         """Тест на невозможность удаления чужой заметки."""
-        # Создаем заметку для второго пользователя
-        foreign_note = Note.objects.create(
-            title='Чужая заметка',
-            text='Содержание чужой заметки',
-            author=self.user2)  # Указываем автора заметки
-        # Авторизуем первого пользователя
-        self.client.force_login(self.user1)
         # Пытаемся удалить чужую заметку
-        response = self.client.post(
-            reverse('notes:delete', args=[foreign_note.id]),
+        response = self.clientSecond.post(
+            self.urls_test_anonymous_access['notes:delete'],
             follow=True)
         # Проверяем, что заметка не была удалена
-        self.assertEqual(response.status_code, 404)
-        self.assertTrue(Note.objects.filter(id=foreign_note.id).exists())
+        self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
+        self.assertTrue(Note.objects.filter(
+            id=self.first_note_userFirstAuthorized.id).exists())
 
 
 if __name__ == '__main__':
     unittest.main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # ---------
+# import unittest
+# from django.urls import reverse
+# from django.contrib.auth.models import User
+# from django.test import Client, TestCase
+# from notes.models import Note
+# from pytils.translit import slugify
+
+
+# class NoteLogicTests(TestCase):
+
+#     def setUp(self):
+#         """Создание тестовых пользователей."""
+#         self.user1 = User.objects.create_user(
+#             username='User1', password='User1_123')
+#         self.user2 = User.objects.create_user(
+#             username='User2', password='User2_123')
+
+#         """Создание клиента и авторизация пользователя."""
+#         self.client = Client()
+#         self.client.force_login(self.user1)
+
+#         """Создание тестовую заметку."""
+#         self.note = Note.objects.create(
+#             title='Тест',
+#             text='Содержание теста',
+#             author=self.user1)
+
+#     def test_create_note_authenticated(self):
+#         """Тест на создание заметки авторизованным пользователем."""
+#         # Проверяем начальное количество заметок
+#         self.assertEqual(Note.objects.count(), 1)
+#         response = self.client.post(reverse('notes:add'), data={
+#             'title': 'Новая заметка',
+#             'text': 'Содержание новой заметки'})
+#         self.assertEqual(response.status_code, 302)
+#         self.assertEqual(Note.objects.count(), 2)
+#         # Проверяем создание новой заметки
+#         new_note = Note.objects.last()
+#         self.assertEqual(new_note.title, 'Новая заметка')
+#         self.assertEqual(new_note.text, 'Содержание новой заметки')
+#         self.assertEqual(new_note.author, self.user1)
+#         # print('Good')
+
+#     def test_create_note_anonymous(self):
+#         """Тест на невозможность создания заметки анонимным пользователем."""
+#         anon_client = Client()
+#         response = anon_client.post(reverse('notes:add'), {
+#             'title': 'Новая заметка',
+#             'text': 'Содержание новой заметки'})
+#         self.assertEqual(response.status_code, 302)
+#         self.assertEqual(Note.objects.count(), 1)
+
+#     def test_unique_slug(self):
+#         """Тест на уникальность slug."""
+#         # Попытка создать заметку с тем же slug
+#         with self.assertRaises(Exception):
+#             Note.objects.create(
+#                 title=self.note.title,
+#                 text='Другое содержание',
+#                 author=self.user1,
+#                 slug=self.note.slug)
+
+#     def test_auto_slug(self):
+#         """Тест на автоматическое формирование slug."""
+#         response = self.client.post(reverse('notes:add'), {
+#             'title': 'Тестовая заметка',
+#             'text': 'Содержание'})
+#         self.assertEqual(response.status_code, 302)
+#         new_note = Note.objects.last()
+#         self.assertEqual(new_note.slug, slugify('тестовая-заметка'))
+
+#     def test_edit_own_note(self):
+#         """Тест на редактирование своей заметки."""
+#         response = self.client.post(reverse(
+#             'notes:edit', args=[self.note.slug]), {
+#             'title': 'Измененная заметка',
+#             'text': 'Новое содержание'})
+#         self.assertEqual(response.status_code, 302)
+#         self.note.refresh_from_db()
+#         self.assertEqual(self.note.title, 'Измененная заметка')
+
+#     def test_edit_foreign_note(self):
+#         """Тест на невозможность редактирования чужой заметки."""
+#         # Создаем заметку для второго пользователя
+#         foreign_note = Note.objects.create(
+#             title='Чужая заметка',
+#             text='Содержание чужой заметки',
+#             author=self.user2)
+#         response = self.client.post(reverse(
+#             'notes:edit', args=[foreign_note.slug]), {
+#             'title': 'Попытка изменения'})
+#         self.assertEqual(response.status_code, 404)  # Запрещено
+
+#     def test_delete_own_note(self):
+#         """Тест на удаление своей заметки."""
+#         response = self.client.post(reverse(
+#             'notes:delete', args=[self.note.slug]))
+#         self.assertEqual(response.status_code, 302)
+#         self.assertEqual(Note.objects.count(), 0)
+
+#     def test_delete_foreign_note(self):
+#         """Тест на невозможность удаления чужой заметки."""
+#         # Создаем заметку для второго пользователя
+#         foreign_note = Note.objects.create(
+#             title='Чужая заметка',
+#             text='Содержание чужой заметки',
+#             author=self.user2)  # Указываем автора заметки
+#         # Авторизуем первого пользователя
+#         self.client.force_login(self.user1)
+#         # Пытаемся удалить чужую заметку
+#         response = self.client.post(
+#             reverse('notes:delete', args=[foreign_note.id]),
+#             follow=True)
+#         # Проверяем, что заметка не была удалена
+#         self.assertEqual(response.status_code, 404)
+#         self.assertTrue(Note.objects.filter(id=foreign_note.id).exists())
+
+
+# if __name__ == '__main__':
+#     unittest.main()
