de45ea3bbb7340c4222b287f8ff55de70e9a04f9
vodoleikin <vodoleikin@yandex.ru>
2025-10-08 11:06:51 +0300

    08.10.2025. 11.05. v7. All tests passed. Preparing to readd ya_news/news/pytest_tests/conftest.py, ya_news/news/pytest_tests/test_routes.py.

--- ya_news/news/pytest_tests/conftest.py
+++ ya_news/news/pytest_tests/conftest.py
@@ -113,3 +113,128 @@ def login_url():
 def signup_url():
     """URL страницы регистрации."""
     return reverse('users:signup')
+
+
+
+
+
+
+
+
+
+# # ==========
+# import datetime
+# from time import timezone
+
+# import pytest
+# from django.conf import settings
+# from django.test import Client
+# from django.urls import reverse
+# from django.utils import timezone
+
+# from news.models import Comment, News
+# from .const import COUNT_OF_COMMENTS
+
+
+# @pytest.fixture
+# def author(django_user_model):
+#     """Создание автора."""
+#     return django_user_model.objects.create(username='Автор')
+
+
+# @pytest.fixture
+# def another_user(django_user_model):
+#     """Создаёт стороннего пользоваетля."""
+#     return django_user_model.objects.create(username='Сторонний пользователь')
+
+
+# @pytest.fixture
+# def author_client(author):
+#     """Авторизация автора."""
+#     client = Client()
+#     client.force_login(author)
+#     return client
+
+
+# @pytest.fixture
+# def another_user_client(another_user):
+#     """Авторизация стороннего пользователя."""
+#     client = Client()
+#     client.force_login(another_user)
+#     return client
+
+
+# @pytest.fixture
+# def news(db):
+#     """Создание новости."""
+#     return News.objects.create(
+#         title='Заголовок',
+#         text='Текст')
+
+
+# @pytest.fixture
+# def comment(news, author):
+#     """Создание комментария."""
+#     comment = Comment.objects.create(
+#         news=news,
+#         text='Текст комментария',
+#         author=author,)
+#     return comment
+
+
+# @pytest.fixture
+# def news_list(db):
+#     """Создание списка новостей."""
+#     today = datetime.date.today()
+#     for index in range(settings.NEWS_COUNT_ON_HOME_PAGE + 1):
+#         News.objects.create(
+#             title=f'Еще одна свежая новость {index}',
+#             text='Текст еще одной свежей новости.',
+#             date=today - datetime.timedelta(days=index))
+
+
+# @pytest.fixture
+# def comments_list(author, news):
+#     """Создание списка комментариев."""
+#     now = timezone.now()
+#     for index in range(COUNT_OF_COMMENTS):
+#         comment = Comment.objects.create(
+#             news=news, author=author, text=f'Tекст {index}',)
+#         comment.created = now + datetime.timedelta(days=index)
+#         comment.save()
+
+
+# @pytest.fixture
+# def home_url():
+#     """URL главной страницы."""
+#     return reverse('news:home')
+
+
+# @pytest.fixture
+# def detail_url(news):
+#     """URL страницы новости."""
+#     return reverse('news:detail', args=(news.id,))
+
+
+# @pytest.fixture
+# def edit_url(comment):
+#     """URL редактирования комментария."""
+#     return reverse('news:edit', args=(comment.id,))
+
+
+# @pytest.fixture
+# def delete_url(comment):
+#     """URL удаления комментария."""
+#     return reverse('news:delete', args=(comment.id,))
+
+
+# @pytest.fixture
+# def login_url():
+#     """URL страницы входа."""
+#     return reverse('users:login')
+
+
+# @pytest.fixture
+# def signup_url():
+#     """URL страницы регистрации."""
+#     return reverse('users:signup')
--- ya_news/news/pytest_tests/test_routes.py
+++ ya_news/news/pytest_tests/test_routes.py
@@ -38,25 +38,20 @@ class TestRoutes:
         assert response.status_code == HTTPStatus.OK
 
     def test_anonymous_news_detail(self):
-        response = self.client.get(reverse(
-            'news:detail', args=[self.news.id]))
+        response = self.client.get(reverse('news:detail', args=[self.news.id]))
         assert response.status_code == HTTPStatus.OK
 
     def test_anonymous_comment_edit_redirect(self):
         url_login = reverse('users:login')
-        response = self.client.get(reverse(
-            'news:edit', args=[self.comment.id]))
+        response = self.client.get(reverse('news:edit', args=[self.comment.id]))
         assert response.status_code == HTTPStatus.FOUND
-        assert response.url == f'{url_login}?next={reverse(
-            "news:edit", args=[self.comment.id])}'
+        assert response.url == f'{url_login}?next={reverse('news:edit', args=[self.comment.id])}'
 
     def test_anonymous_comment_delete_redirect(self):
         url_login = reverse('users:login')
-        response = self.client.get(reverse(
-            'news:delete', args=[self.comment.id]))
+        response = self.client.get(reverse('news:delete', args=[self.comment.id]))
         assert response.status_code == HTTPStatus.FOUND
-        assert response.url == f'{url_login}?next={reverse(
-            "news:delete", args=[self.comment.id])}'
+        assert response.url == f'{url_login}?next={reverse('news:delete', args=[self.comment.id])}'
 
     def test_anonymous_auth_pages(self):
         login_response = self.client.get(reverse('users:login'))
@@ -69,14 +64,12 @@ class TestRoutes:
     # Тесты для авторизованного пользователя
     def test_authorized_comment_edit(self):
         self.client.force_login(self.user)
-        response = self.client.get(reverse(
-            'news:edit', args=[self.comment.id]))
+        response = self.client.get(reverse('news:edit', args=[self.comment.id]))
         assert response.status_code == HTTPStatus.OK
 
     def test_authorized_comment_delete(self):
         self.client.force_login(self.user)
-        response = self.client.get(reverse(
-            'news:delete', args=[self.comment.id]))
+        response = self.client.get(reverse('news:delete', args=[self.comment.id]))
         assert response.status_code == HTTPStatus.OK
 
     def test_authorized_foreign_comment_edit(self):
@@ -90,8 +83,7 @@ class TestRoutes:
             text='Other user comment'
         )
         self.client.force_login(self.user)
-        response = self.client.get(reverse(
-            'news:edit', args=[other_comment.id]))
+        response = self.client.get(reverse('news:edit', args=[other_comment.id]))
         assert response.status_code == HTTPStatus.NOT_FOUND
 
     def test_authorized_foreign_comment_delete(self):
@@ -105,6 +97,117 @@ class TestRoutes:
             text='Another user comment'
         )
         self.client.force_login(self.user)
-        response = self.client.get(reverse(
-            'news:delete', args=[other_comment.id]))
+        response = self.client.get(reverse('news:delete', args=[other_comment.id]))
         assert response.status_code == HTTPStatus.NOT_FOUND
+
+
+
+
+
+
+
+
+
+
+# # ==========
+# from http import HTTPStatus
+# import pytest
+
+# from django.contrib.auth import get_user_model
+# from django.test import Client
+# from django.urls import reverse
+
+# from news.models import Comment, News
+
+# User = get_user_model()
+
+# pytestmark = pytest.mark.django_db
+
+
+# class TestRoutes:
+
+#     @pytest.fixture(autouse=True)
+#     def setup(self):
+#         self.client = Client()
+#         self.user = User.objects.create_user(
+#             username='User1',
+#             password='User1password',
+#             is_active=True
+#         )
+#         self.news = News.objects.create(
+#             title='Test News',
+#             text='Content of the test news'
+#         )
+#         self.comment = Comment.objects.create(
+#             news=self.news,
+#             author=self.user,
+#             text='Test comment'
+#         )
+
+#     # Тесты для анонимного пользователя
+#     def test_anonymous_home_page(self):
+#         response = self.client.get(reverse('news:home'))
+#         assert response.status_code == HTTPStatus.OK
+
+#     def test_anonymous_news_detail(self):
+#         response = self.client.get(reverse('news:detail', args=[self.news.id]))
+#         assert response.status_code == HTTPStatus.OK
+
+#     def test_anonymous_comment_edit_redirect(self):
+#         url_login = reverse('users:login')
+#         response = self.client.get(reverse('news:edit', args=[self.comment.id]))
+#         assert response.status_code == HTTPStatus.FOUND
+#         assert response.url == f'{url_login}?next={reverse('news:edit', args=[self.comment.id])}'
+
+#     def test_anonymous_comment_delete_redirect(self):
+#         url_login = reverse('users:login')
+#         response = self.client.get(reverse('news:delete', args=[self.comment.id]))
+#         assert response.status_code == HTTPStatus.FOUND
+#         assert response.url == f'{url_login}?next={reverse('news:delete', args=[self.comment.id])}'
+
+#     def test_anonymous_auth_pages(self):
+#         login_response = self.client.get(reverse('users:login'))
+#         assert login_response.status_code == HTTPStatus.OK
+#         signup_response = self.client.get(reverse('users:signup'))
+#         assert signup_response.status_code == HTTPStatus.OK
+#         logout_response = self.client.post(reverse('users:logout'))
+#         assert logout_response.status_code == HTTPStatus.OK
+
+#     # Тесты для авторизованного пользователя
+#     def test_authorized_comment_edit(self):
+#         self.client.force_login(self.user)
+#         response = self.client.get(reverse('news:edit', args=[self.comment.id]))
+#         assert response.status_code == HTTPStatus.OK
+
+#     def test_authorized_comment_delete(self):
+#         self.client.force_login(self.user)
+#         response = self.client.get(reverse('news:delete', args=[self.comment.id]))
+#         assert response.status_code == HTTPStatus.OK
+
+#     def test_authorized_foreign_comment_edit(self):
+#         other_user = User.objects.create_user(
+#             username='anotheruser',
+#             password='anotherpassword'
+#         )
+#         other_comment = Comment.objects.create(
+#             news=self.news,
+#             author=other_user,
+#             text='Other user comment'
+#         )
+#         self.client.force_login(self.user)
+#         response = self.client.get(reverse('news:edit', args=[other_comment.id]))
+#         assert response.status_code == HTTPStatus.NOT_FOUND
+
+#     def test_authorized_foreign_comment_delete(self):
+#         other_user = User.objects.create_user(
+#             username='anotheruser',
+#             password='anotherpassword'
+#         )
+#         other_comment = Comment.objects.create(
+#             news=self.news,
+#             author=other_user,
+#             text='Another user comment'
+#         )
+#         self.client.force_login(self.user)
+#         response = self.client.get(reverse('news:delete', args=[other_comment.id]))
+#         assert response.status_code == HTTPStatus.NOT_FOUND
